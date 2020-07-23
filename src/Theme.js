8905bac5d1fb091533712f27dc6055528d437799
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2020-07-22 21:50:26 -0400

    Added brown theme

---
+++ src/Theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import brown from '@material-ui/core/colors/brown';
+
+export default createMuiTheme({
+	palette: {
+		primary: brown,
+	},
+});
